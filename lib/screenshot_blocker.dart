bb3bc573cbd28a9353a7818c1e2b47516d50b372
Nishat <104446439+zas-nishat@users.noreply.github.com>
2025-06-05 15:22:28 +0600

    dart formatted

--- lib/screenshot_blocker.dart
+++ lib/screenshot_blocker.dart
@@ -3,7 +3,9 @@ import 'package:flutter/services.dart';
 
 class ScreenshotBlocker {
   static const MethodChannel _channel = MethodChannel('screenshot_blocker');
-  static const EventChannel _eventChannel = EventChannel('screenshot_blocker_events');
+  static const EventChannel _eventChannel = EventChannel(
+    'screenshot_blocker_events',
+  );
 
   static Future<void> secureScreen() async {
     await _channel.invokeMethod('secureScreen');
